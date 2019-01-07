cc3c77f1b59064e9f44d7c34c20829a92b67143f
Sergey Onufrienko <sergey@onufrienko.com>
2019-01-07 21:29:17 +0200

    Add more tests

--- load-test.sh
+++ load-test.sh
@@ -1,16 +1,54 @@
 #!/bin/bash
 
 URL="http://<ip>/books"
+URL2="http://<ip>/books/one"
+URL3="http://<ip>/books/none"
 
 sleep 5
-wrk -d10m -c10 -t2 $URL
+wrk -d2m -c10 -t2 $URL
 sleep 5
-wrk -d10m -c100 -t2 $URL
+wrk -d2m -c100 -t2 $URL
 sleep 5
-wrk -d10m -c1000 -t2 $URL
+wrk -d2m -c500 -t2 $URL
 sleep 5
-wrk -d10m -c1000 -t2 -s ./post.lua $URL
+wrk -d2m -c1000 -t2 $URL
 sleep 5
-wrk -d10m -c1000 -t2 -s ./post.lua $URL
+wrk -d2m -c2000 -t2 $URL
 sleep 5
-wrk -d10m -c1000 -t2 -s ./post.lua $URL
+wrk -d2m -c4000 -t2 $URL
+sleep 5
+wrk -d2m -c10 -t2 $URL2
+sleep 5
+wrk -d2m -c100 -t2 $URL2
+sleep 5
+wrk -d2m -c500 -t2 $URL2
+sleep 5
+wrk -d2m -c1000 -t2 $URL2
+sleep 5
+wrk -d2m -c2000 -t2 $URL2
+sleep 5
+wrk -d2m -c4000 -t2 $URL2
+sleep 5
+wrk -d2m -c10 -t2 $URL3
+sleep 5
+wrk -d2m -c100 -t2 $URL3
+sleep 5
+wrk -d2m -c500 -t2 $URL3
+sleep 5
+wrk -d2m -c1000 -t2 $URL3
+sleep 5
+wrk -d2m -c2000 -t2 $URL3
+sleep 5
+wrk -d2m -c4000 -t2 $URL3
+sleep 5
+wrk -d2m -c10 -t2 -s ./post.lua $URL
+sleep 5
+wrk -d2m -c100 -t2 -s ./post.lua $URL
+sleep 5
+wrk -d2m -c500 -t2 -s ./post.lua $URL
+sleep 5
+wrk -d2m -c1000 -t2 -s ./post.lua $URL
+sleep 5
+wrk -d2m -c2000 -t2 -s ./post.lua $URL
+sleep 5
+wrk -d2m -c4000 -t2 -s ./post.lua $URL
